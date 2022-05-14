51e955937e191783d10e539bc3de8b10b09d9ce9
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-05-14 11:45:24 +0100

    Create a grocery list

---
+++ src/components/List.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FaTrash, FaEdit } from "react-icons/fa";
+
+const List = ({ items, removeItem, editItem }) => {
+  return (
+    <div className="mx-2 my-4">
+      {
+        items.map((item) => {
+          const { id, title } = item;
+          return (
+            <article 
+              key={id}
+              className="flex place-content justify-between py-2 border-b-2"
+            >
+              <p className="capitalize">{title}</p>
+              <div className="flex text-xs">
+                <button
+                  className="mr-2 text-green-600"
+                  onClick={() => editItem(id)}
+                >
+                  <FaEdit />
+                </button>
+                <button
+                  className="text-red-400"
+                  onClick={() => removeItem(id)}
+                >
+                  <FaTrash />
+                </button>
+              </div>
+            </article>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default List;
